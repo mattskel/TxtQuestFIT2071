dd37e330be8a61e9206eac94755dc6e0f3b2631d
Matthew Skelley <matthewskelley@matthews-macbook-pro.local>
2016-05-15 07:48:05 +1000

    Initial Commit

---
+++ Assignment03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment03
+//
+//  Created by Matthew Skelley on 15/05/2016.
+//  Copyright © 2016 Matthew Skelley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
